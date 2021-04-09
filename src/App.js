c86b83c7ebe43896d2a57bf6f5ec1f10ef295296
dbudak <dmitriybudak@gmail.com>
2021-04-08 22:59:22 -0500

    added router, removed more cli stuff

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import './App.css';
+import HomeComponent from './components/HomeComponent';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      foo bar
-    </div>
+    <Router>
+      <Switch>
+        <Route path='/'>
+          <HomeComponent></HomeComponent>
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
